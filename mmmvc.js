99b63600aad49f628eceb8a2fd3586e6af0e48d2
mil <miles.sandlar@gmail.com>
2014-01-29 19:57:23 -0500

    set_action passed {}. set multiple actions at once

--- mmmvc.js
+++ mmmvc.js
@@ -1,8 +1,11 @@
 var Mmmvc = (function(my) {
-  var add_action = function() {}, 
-  remove_action = function() {}, 
-  empty_action = function() {},
-  populate_action = function() {}, 
+  var actions = {
+    add      : function() {},
+    remove   : function() {},
+    update   : function() {},
+    populate : function() {},
+    empty    : function() {}
+  }, 
   model = {};
 
   function sync(hash_with_unique_keys) { 
@@ -13,27 +16,29 @@ var Mmmvc = (function(my) {
       _.each(_.keys(model[k]), function(key) {
         if (model[k][key] != hash_with_unique_keys[k][key]) {
           model[k] = hash_with_unique_keys[k];
-          update_action(k,model[k]);
+          actions['update'](k,model[k]);
           return;
         }
       });
     });
 
-    if (_.size(hash_with_unique_keys) == 0 && _.size(model) != 0) { empty_action(); }
-    if (_.size(model) == 0 && _.size(hash_with_unique_keys) != 0) { populate_action(); }
+    if (_.size(hash_with_unique_keys) == 0 && _.size(model) != 0) { actions['empty'](); }
+    if (_.size(model) == 0 && _.size(hash_with_unique_keys) != 0) { actions['populate'](); }
 
-    _.each(add_items, function(value, key) { add_action(key, value); });
-    _.each(remove_items, function(value, key) { remove_action(key, value) });
+    _.each(add_items, function(value, key) { actions['add'](key, value); });
+    _.each(remove_items, function(value, key) { actions['remove'](key, value) });
 
-    model = hash_with_unique_keys;
+    model = _.clone(hash_with_unique_keys);
+    return true;
   }
   return {
-    set_empty_action    : function(action) { empty_action    = action; },
-    set_populate_action : function(action) { populate_action = action; },
-    set_add_action      : function(action) { add_action      = action; },
-    set_remove_action   : function(action) { remove_action   = action; },
-    set_update_action   : function(action) { update_action   = action; },
-    sync : sync,
-    initialize : function() { empty_action(); }
+    initialize : function() { actions.empty(); },
+    set_action : function(new_hash) {
+      if (actions['add']) { actions.add = new_hash.add; }
+      if (actions['remove']) { actions.remove = new_hash.remove; }
+      if (actions['populate']) { actions.populate = new_hash.populate; }
+      if (actions['empty']) { actions.empty = new_hash.empty; }
+    },
+    sync : sync
   };
 });
