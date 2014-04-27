666ce0518a7c9d1174f308fcc84b82d4eb1c2349
mil <miles.sandlar@gmail.com>
2014-04-27 16:15:45 -0400

    activate_code_window working

--- demo/js/demo_page.js
+++ demo/js/demo_page.js
@@ -1,3 +1,4 @@
+var code_window = null;
 function pp_model() {
   function spaces(number) {
     var sp = "";
@@ -28,16 +29,34 @@ function pp_model() {
 
   return recurse_obj(todo_model, 0);
 }
-function activate_code_window(code_true_implementation_false) {
+function spawn_code_window() {
   var content = $("#code").html();
-  var new_window = open('', 'Mmvp.js Source', 'height=600,width=600');
-  new_window.document.write(content);
+  code_window = open('', 'Mmvp.js Source', 'height=720,width=720');
+  code_window.document.write(content);
+  $("body", code_window.document).attr("id", "code-window");
+  $("h3", code_window.document).on("click", function(ev) {
+    if ($(ev.target).text() == "Todo.js") {
+      activate_code_window("todo");
+    } else {
+      activate_code_window("mmvp");
+    }
+  });
+}
+function activate_code_window(mmvp_or_todo) {
+  if (!code_window) { spawn_code_window(); } 
+  if (mmvp_or_todo === 'todo') {
+    $("body", code_window.document).addClass("todo").removeClass("mmvp");
+  } else if (mmvp_or_todo === 'mmvp') {
+    $("body", code_window.document).addClass("mmvp").removeClass("todo");
+  }
 }
 $(function() {
-  (function syntax_highlighter() {
-    SyntaxHighlighter.all();
+  (function code_window_callbacks() { 
+    $("a#todo-src").on("click", function() { activate_code_window('todo'); return false; });
+    $("a#mmvp-src").on("click", function() { activate_code_window('mmvp'); return false; });
   })();
-  (function tab_switcher() {
+  (function syntax_highlighter() { SyntaxHighlighter.all(); })();
+  (function tab_switcher_callbacks() {
     $("section#demo h3").on("click", function(ev) {
       if (!$(ev.target).hasClass('active')) {
 
@@ -47,7 +66,7 @@ $(function() {
 
         var new_tab = $(ev.target).attr('class');
         if (new_tab == 'model') {
-          $("section.model").html("<pre class='brush: js;'>" + pp_model() + "</pre>");
+          $("section.model").html("<pre class='brush: js; toolbar: false;'>" + pp_model() + "</pre>");
           SyntaxHighlighter.highlight();
         }
 
