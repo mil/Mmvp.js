3a091717e38995d6d9c52ac98c2c29400191cd40
mil <miles.sandlar@gmail.com>
2014-04-27 20:51:29 -0400

    links to raw src

--- demo/js/todo.js
+++ demo/js/todo.js
@@ -1,3 +1,4 @@
+// sourced: http://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript
 function uuid() {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
     var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
@@ -5,28 +6,34 @@ function uuid() {
   });
 }
 
+var todo_model = {}, 
+    presenter_dom_identifier = "#viewpen", 
+    todo_presenter = new Mmvp();
+
 // Setting up the presenter callbacks
-var todo_model = {}, presenter_dom_identifier = "#viewpen", todo_presenter = new Mmvp();
 todo_presenter.set_action({
   empty : function() {
     $("#items-container").remove();
-    $(presenter_dom_identifier).append(
-      "<div id='no-items'><h4>No Items!</h4><p>Hint: Add a new item.</p></div>"
-    );
-    $("#no-items").animate({opacity : 1 }, 300, 'ease-in');
+    var no_items_el = $("<div id='no-items'>");
+    no_items_el.append("<h4>No Items!</h4>").append("<p>Hint: Add a new item.</p>");
+    $(presenter_dom_identifier).append(no_items_el);
+    no_items_el.animate({opacity : 1 }, 300, 'ease-in');
   },
   populate : function() {
     $("#no-items").remove(); 
-    $(presenter_dom_identifier).append("<div id='items-container'></div>");
+    $(presenter_dom_identifier).append("<div id='items-container'>");
   },
   add : function(new_model_key, new_model_value) {
     var checked = new_model_value == true ? "checked" : "";
-    var new_div = $("<div><input type='checkbox' " + checked + "><span>" + new_model_value.text + "</span></div>");
-    new_div.attr('id', new_model_key).prepend(
-      $("<button class='remove'>&#150;</button>")
-    );
-    $("#items-container").prepend(new_div);
-    new_div.animate({ opacity: 1 }, 300, 'ease-in');
+    var new_div_el = $("<div>");
+    var checkbox = $("<input type='checkbox'>");
+    checkbox.attr("checked", (checked ? "" : null));
+    var span = $("<span>").text(new_model_value.text);
+    var remove_btn = $("<button class='remove'>").html("&#150;");
+    new_div_el.attr('id', new_model_key).append(checkbox, span, remove_btn);
+
+    $("#items-container").prepend(new_div_el);
+    new_div_el.animate({ opacity: 1 }, 300, 'ease-in');
   },
   remove : function(key_of_removed_item) {
     $("#" + key_of_removed_item).remove();
