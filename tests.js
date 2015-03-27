47ef2e04ee6ebbfa8802726052550e92f7e14d4d
mil <miles.sandlar@gmail.com>
2015-03-26 19:46:14 -0500

    some tests

--- tests.js
+++ tests.js
@@ -1,5 +1,8 @@
 var test = require('tape');
+var tap_spec = require('tap-spec');
 var mmvp = require('./index.js');
+
+test.createStream().pipe(tap_spec()).pipe(process.stdout);
 
 var delay_ms = 300;
 test('mmvp.initialize runs empty action', function(t) {
@@ -15,19 +18,26 @@ test('mmvp.initialize runs empty action', function(t) {
   p.initialize();
 });
 
-test('adding an item triggers current add_action', function(t) {
-  t.plan(2);
+test('add and populate item triggers current add_action', function(t) {
+  t.plan(3);
   var p = new mmvp();
-  var expect_key, expect_value;
+  var expect_add_key, expect_add_value;
+  var populate_fn_has_run = false;
+
   p.set_action({
    add: function(key, value) {
       expect_key = 'mykey';
       expect_value = 'myvalue';
+    },
+    populate: function() {
+      populate_fn_has_run = true;
     }
   });
+
   setTimeout(function() { 
-    t.equal(expect_key, 'mykey');
-    t.equal(expect_value, 'myvalue'); 
+    t.equal(expect_key, 'mykey', 'Add fn was triggered with proper key');
+    t.equal(expect_value, 'myvalue', 'Add fn was triggered with proper value'); 
+    t.equal(populate_fn_has_run, true, 'Populate function fn runs when model becomes populated');
   }, delay_ms);
   p.initialize();
   p.sync({ 'mykey': 'myvalue' });
