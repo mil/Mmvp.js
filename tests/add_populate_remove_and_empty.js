2c342e70da805fa3d973439f1c4066dd30e6db00
mil <miles.sandlar@gmail.com>
2015-03-29 20:59:34 -0500

    add tests

--- tests/add_populate_remove_and_empty.js
+++ tests/add_populate_remove_and_empty.js
@@ -1,29 +1,45 @@
 module.exports = (function(test, mmvp, delay_ms) {
-
-
-  test('add and populate item triggers current add_action', function(t) {
-    t.plan(3);
-
+  test('add,populate, remove, and empty callbacks work', function(t) {
+    t.plan(4);
     var p = new mmvp();
     var expect_add_key, expect_add_value;
+    var expect_remove_key, expect_remove_value;
     var populate_fn_has_run = false;
+    var empty_fn_has_run = false;
+    var add_key = 'mykey';
+    var add_value = 'myvalue';
 
     p.set_action({
       add: function(key, value) {
-        expect_key = 'mykey';
-        expect_value = 'myvalue';
+        expect_add_key = key;
+        expect_add_value = value;
       },
       populate: function() {
         populate_fn_has_run = true;
+      },
+      empty: function() {
+        if (!populate_fn_has_run) { return; }
+        empty_fn_has_run = true;
+      },
+      remove: function(key, value) {
+        expect_remove_key = key;
+        expect_remove_value = value;
       }
     });
 
     setTimeout(function() { 
+      console.log(expect_key);
       t.equal(expect_key, 'mykey', 'Add fn was triggered with proper key');
       t.equal(expect_value, 'myvalue', 'Add fn was triggered with proper value'); 
+
+
+
       t.equal(populate_fn_has_run, true, 'Populate function fn runs when model becomes populated');
+      t.equal(empty_fn_has_run, true, 'Empty fn runs when model becomes empty');
+
     }, delay_ms);
     p.initialize();
     p.sync({ 'mykey': 'myvalue' });
+    p.sync({});
   });
 });
--- tests/run_tests.js
+++ tests/run_tests.js
@@ -9,3 +9,4 @@ if (process.argv[2] == '--with-color') {
 var delay_ms = 300;
 require('./initialize.js')(test, mmvp, delay_ms);
 require('./add_and_populate.js')(test, mmvp, delay_ms);
+require('./add_populate_remove_and_empty.js')(test, mmvp, delay_ms);
