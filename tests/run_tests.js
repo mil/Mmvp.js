deedcb3f9eb31824c56bf2d71594b417ee025eba
mil <miles.sandlar@gmail.com>
2015-03-29 21:21:24 -0500

    less interfaces

--- tests/run_tests.js
+++ tests/run_tests.js
@@ -1,12 +1,7 @@
 var test = require('tape');
 var mmvp = require('../index.js');
-
-if (process.argv[2] == '--with-color') {
-  var tap_spec = require('tap-spec');
-  test.createStream().pipe(tap_spec()).pipe(process.stdout);
-}
-
 var delay_ms = 300;
+
 require('./initialize.js')(test, mmvp, delay_ms);
 require('./add_and_populate.js')(test, mmvp, delay_ms);
 require('./add_populate_remove_and_empty.js')(test, mmvp, delay_ms);
